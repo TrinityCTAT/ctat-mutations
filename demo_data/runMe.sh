88bd602d581ce71888f1030ff5a844c904b4640d
Brian Haas <brianjohnhaas@gmail.com>
2018-08-20 12:00:05 -0400

    adding new sample data set for testing

--- demo_data/runMe.sh
+++
@@ -1,1 +0,0 @@
-../ctat_mutations --left FLI1.left.fq --right FLI1.right.fq --out_dir test.varcalling
---
+++ testing/runMe.sh
@@ -0,0 +1,1 @@
+../ctat_mutations --left reads_1.fastq.gz --right reads_2.fastq.gz --out_dir example.varcalling
