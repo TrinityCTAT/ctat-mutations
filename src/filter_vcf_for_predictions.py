52844118197347a4aacc76a0b7ae58b8fade033d
bankapur <bankapur@delanatii.broadinstitute.org>
2019-01-23 10:43:45 -0500

    COSMIC pulling CANCER snps

--- src/filter_vcf_for_predictions.py
+++ src/filter_vcf_for_predictions.py
@@ -12,6 +12,7 @@ CHR_COMMENT = "#"
 STR_VCF_DELIMITER = "\t"
 I_INFO_INDEX = 7
 CHR_INFO_DELIMITER = ";"
+
 # INFO features
 STR_CHASM_FDR = "CHASM_FDR"
 STR_CHASM_PVALUE = "CHASM_PVALUE"
@@ -19,7 +20,10 @@ STR_VEST_FDR = "VEST_FDR"
 STR_VEST_PVALUE = "VEST_PVALUE"
 STR_COSMIC_ID = "COSMIC_ID"
 STR_FATHMM = "FATHMM"
-STR_CANCER = "CANCER"
+STR_CANCER = ["CANCER","PATHOGENIC"]
+STR_TISSUE = "TISSUE"
+
+
 # Thresholds
 I_FDR = 0.3
 I_PVALUE = 0.05
@@ -31,6 +35,7 @@ prsr_arguments = argparse.ArgumentParser(prog="filter_vcf_for_predictions.py",
                                          formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 prsr_arguments.add_argument("str_input_file", help="Input vcf.gz file.")
 prsr_arguments.add_argument("str_output_file", help="Output filtered vcf file.")
+#prsr_arguments.add_argument("tissue_type", help="Tissue type.")
 args = prsr_arguments.parse_args()
 
 # Stores the vcf info
@@ -74,7 +79,8 @@ if args.str_input_file:
             # Parse INFO tokens
             dict_info_tokens = dict([func_split_token(str_token) for str_token
                                     in lstr_line[I_INFO_INDEX].split(CHR_INFO_DELIMITER)])
-            print dict_info_tokens
+            #print dict_info_tokens
+
             # Keep everything that is a COSMIC ID at this point.
             # Otherwise require CRAVAT or VEST to have an annotation.
             if STR_CHASM_FDR in dict_info_tokens:
@@ -88,9 +94,12 @@ if args.str_input_file:
             elif(float(dict_info_tokens.get(STR_VEST_PVALUE, "2")) <= I_PVALUE):
                 f_keep = True
 
-            # Keep FATHMM = Cancer
+            #print dict_info_tokens
+
+            # Keep FATHMM = Cancer and FATHMM=PATHOGENIC
             if( STR_COSMIC_ID in dict_info_tokens and
-                dict_info_tokens.get(STR_FATHMM, "") == STR_CANCER ):
+                dict_info_tokens.get(STR_FATHMM, "") in STR_CANCER):
+                #and dict_info_tokens.get(STR_TISSUE, "") == args.tissue_type):
                 f_keep = True
 
             # Store passing variant
