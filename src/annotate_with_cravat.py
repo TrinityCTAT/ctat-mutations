41da6bf6900a2a78c1db4b911430a835ab025b9d
Brian Haas <brianjohnhaas@gmail.com>
2020-07-08 14:45:17 -0400

    upgrades for open-cravat

--- src/annotate_with_cravat.py
+++ src/annotate_with_cravat.py
@@ -1,29 +1,40 @@
 #!/usr/bin/env python
 import argparse
-import os, sys
+import os, sys, re
 import subprocess
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Annotate with Cravat")
-    parser.add_argument("input", help="Input vcf file.")
-    parser.add_argument("output", help="Output base name")
+    parser.add_argument("--input_vcf", help="Input vcf file.", required=True)
+    parser.add_argument("--output_vcf", help="Output cravat-annotated vcf file", required=True)
 
-    parser.add_argument("--genome", help="Genome", default='hg38')
-    parser.add_argument("--classifier", help="Classifier (ignored)")
-
-    args = parser.parse_args()
-    input_vcf = args.input
-    output = args.output
-
+    parser.add_argument("--genome", help="genome (hg19 or hg38)", default='hg38')
+    parser.add_argument("--cravat_lib_dir", help="cravat resources lib dir", required=True)
     
-
+    args = parser.parse_args()
+    
+    input_vcf = args.input_vcf
+    output_vcf = args.output_vcf
+    cravat_lib_dir = args.cravat_lib_dir
+    
     genome = args.genome
-    output_dir = os.path.dirname(input_vcf)
+    output_dir = os.path.dirname(output_vcf)
+    output_vcf = os.path.basename(output_vcf)
 
+    output_vcf, count = re.subn(".vcf$", "", output_vcf)
+    if count != 1:
+        raise RuntimeError("Error, couldnt replace .vcf extension of {}".format(output_vcf))
+    
+    # set cravat resource lib:
+    subprocess.check_call(["oc", "config", "md", cravat_lib_dir])
+    
     # run open-cravat
-
-    cravat_cmd = ['oc', 'run', '--cs', '{"run":{"vcfreporter":{"type":"separate"}}}', input_vcf, '-t', 'vcf', '-l',
-                  args.genome, '-d', output_dir, '-n', output]
+    cravat_cmd = ['oc', 'run', '--cs', '{"run":{"vcfreporter":{"type":"separate"}}}',
+                  input_vcf,
+                  '-t', 'vcf',
+                  '-l', args.genome,
+                  '-d', output_dir,
+                  '-n', output_vcf]
     
     sys.stderr.write("annotate_with_cravat: {}\n".format(" ".join(cravat_cmd)))
     subprocess.check_call(cravat_cmd)
