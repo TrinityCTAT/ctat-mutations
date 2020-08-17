8a89e7d44d7123c54035cb87c428f9a39ef35b1b
Brian Haas <brianjohnhaas@gmail.com>
2020-08-17 12:50:54 -0400

    ignore unicode utf-8 conversion errors for now

--- PyLib/ctat_util.py
+++ PyLib/ctat_util.py
@@ -5,6 +5,6 @@ import gzip
 # open gzipped or regular text vcf files
 def open_file_for_reading(filename):
     if re.search("\.gz$", filename):
-        return gzip.open(filename, 'rt', encoding="utf-8") # t needed for python3 to look like regular text
+        return gzip.open(filename, 'rt', encoding="utf-8", errors='ignore') # t needed for python3 to look like regular text
     else:
-        return open(filename, 'rt', encoding="utf-8") # regular text file
+        return open(filename, 'rt', encoding="utf-8", errors='ignore') # regular text file
