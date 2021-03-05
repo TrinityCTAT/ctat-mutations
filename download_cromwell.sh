dbe7eb25042bea2db86dc05a05458ea48e3b924c
Brian Haas <brianjohnhaas@gmail.com>
2021-03-05 20:54:22 +0000

    update to download script

--- download_cromwell.sh
+++ download_cromwell.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ ! -f "WDL/cromwell-57.jar" ]]; then
-  wget https://github.com/broadinstitute/cromwell/releases/download/57/cromwell-57.jar -o WDL/cromwell-57.jar
+  wget https://github.com/broadinstitute/cromwell/releases/download/57/cromwell-57.jar -O WDL/cromwell-57.jar
 fi
