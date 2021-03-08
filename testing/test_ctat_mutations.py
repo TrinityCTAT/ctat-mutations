b5a1fe7113bf9010bbd64a0441c016a020d1dd4d
jgould <jgould@broadinstitute.org>
2021-03-08 15:30:02 -0500

    updated tests

--- testing/test_ctat_mutations.py
+++ testing/test_ctat_mutations.py
@@ -7,8 +7,7 @@ from subprocess import check_call
 def check_output(path):
     output_path = os.path.join(path, '**', 'call-VariantFiltration', '*', 'test.filtered.vcf.gz')
     filtered_vcfs = list(glob.iglob(output_path, recursive=True))
-    assert len(filtered_vcfs) == 1, "{} VCFs found".format(len(filtered_vcfs)) + ', '.join(list(
-        glob.iglob(os.path.join(path, '**', 'call-VariantFiltration', '*'), recursive=True)))
+    assert len(filtered_vcfs) == 1, "{} VCFs found".format(len(filtered_vcfs))
     shutil.rmtree(path)
 
 
@@ -28,8 +27,8 @@ def test_benchmarking(tmp_path):
 def test_boosting(boosting_method, boosting_alg_type, tmp_path):
     tmp_path = str(tmp_path)
     check_call(
-        [os.path.abspath('./ctat_mutations'), '--bam', os.path.abspath('testing/reads_output/test.bqsr.bam'), '--vcf',
-         os.path.abspath('testing/reads_output/test.vcf.gz'), '--sample_id', 'test', '--boosting_method',
+        [os.path.abspath('./ctat_mutations'), '--sample_id', 'test', '--boosting_method',
+         '--left', os.path.abspath('testing/reads_1.fastq.gz'), '--right', os.path.abspath('testing/reads_2.fastq.gz'),
          boosting_method, '--boosting_alg_type', boosting_alg_type], cwd=tmp_path)
     check_output(tmp_path)
 
@@ -40,7 +39,7 @@ def test_fastqs(tmp_path):
     check_call(
         [os.path.abspath('./ctat_mutations'), '--left',
          os.path.abspath('testing/reads_1.fastq.gz'), '--right', os.path.abspath('testing/reads_2.fastq.gz'),
-         '--sample_id', 'test', '--boosting_method', 'none', '--read_var_pos_annotation_cpu', '1'], cwd=tmp_path)
+         '--sample_id', 'test', '--boosting_method', 'none'], cwd=tmp_path)
     check_output(tmp_path)
 
 
@@ -50,26 +49,26 @@ def test_single_fastq(tmp_path):
     check_call(
         [os.path.abspath('./ctat_mutations'), '--left',
          os.path.abspath('testing/reads_1.fastq.gz'),
-         '--sample_id', 'test', '--boosting_method', 'none', '--read_var_pos_annotation_cpu', '1'], cwd=tmp_path)
+         '--sample_id', 'test', '--boosting_method', 'none'], cwd=tmp_path)
     check_output(tmp_path)
 
-
-# test the pipeline with pre-aligned bam input and diff with known good output
-def test_bam(tmp_path):
-    tmp_path = str(tmp_path)
-    check_call(
-        [os.path.abspath('./ctat_mutations'), '--bam',
-         os.path.abspath('testing/reads_output/test.star.Aligned.sortedByCoord.out.bam'),
-         '--sample_id', 'test', '--boosting_method',
-         'none', '--read_var_pos_annotation_cpu', '1'], cwd=tmp_path)
-    check_output(tmp_path)
-
-
-# test the pipeline with VCF input, annotate and filter VCF, and diff with known good output
-def test_vcf(tmp_path):
-    tmp_path = str(tmp_path)
-    check_call(
-        [os.path.abspath('./ctat_mutations'), '--bam', os.path.abspath('testing/reads_output/test.bqsr.bam'), '--vcf',
-         os.path.abspath('testing/reads_output/test.vcf.gz'), '--sample_id', 'test', '--boosting_method',
-         'none', '--read_var_pos_annotation_cpu', '1'], cwd=tmp_path)
-    check_output(tmp_path)
+#
+# # test the pipeline with pre-aligned bam input and diff with known good output
+# def test_bam(tmp_path):
+#     tmp_path = str(tmp_path)
+#     check_call(
+#         [os.path.abspath('./ctat_mutations'), '--bam',
+#          os.path.abspath('testing/reads_output/test.star.Aligned.sortedByCoord.out.bam'),
+#          '--sample_id', 'test', '--boosting_method',
+#          'none'], cwd=tmp_path)
+#     check_output(tmp_path)
+#
+#
+# # test the pipeline with VCF input, annotate and filter VCF, and diff with known good output
+# def test_vcf(tmp_path):
+#     tmp_path = str(tmp_path)
+#     check_call(
+#         [os.path.abspath('./ctat_mutations'), '--bam', os.path.abspath('testing/reads_output/test.bqsr.bam'), '--vcf',
+#          os.path.abspath('testing/reads_output/test.vcf.gz'), '--sample_id', 'test', '--boosting_method',
+#          'none'], cwd=tmp_path)
+#     check_output(tmp_path)
