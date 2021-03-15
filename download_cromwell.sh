a07570448549faa157b269dd0dc9478db82d3c18
jgould <jgould@broadinstitute.org>
2021-03-15 14:01:02 -0400

    version 58

--- download_cromwell.sh
+++ download_cromwell.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ ! -f "WDL/cromwell-57.jar" ]]; then
-  wget https://github.com/broadinstitute/cromwell/releases/download/57/cromwell-57.jar -O WDL/cromwell-57.jar
+if [[ ! -f "WDL/cromwell-58.jar" ]]; then
+  wget https://github.com/broadinstitute/cromwell/releases/download/57/cromwell-58.jar -O WDL/cromwell-58.jar
 fi
